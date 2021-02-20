095cc1541f923bf169f6829820eecb330654678b
NictedelRayo <nicteov1912@gmail.com>
2021-02-20 16:47:16 -0500

    Create views.py

---
+++ TutorialDjangoRest/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from series.models import Serie
+from series.serializers import SerieSerializer
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+Ahora, vamos a crear nuestros primeros servicios
